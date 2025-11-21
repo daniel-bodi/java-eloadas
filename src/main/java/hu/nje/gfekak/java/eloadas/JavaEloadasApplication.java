9b7ebd76d18aa5334cf1cdb562b9765c2151aea4
danielbodi <bodi.daniel@hibridlevel.hu>
2025-11-21 15:00:51 +0100

    alap spring boot project

---
+++ src/main/java/hu/nje/gfekak/java/eloadas/JavaEloadasApplication.java
@@ -0,0 +1,12 @@
+package hu.nje.gfekak.java.eloadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEloadasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEloadasApplication.class, args);
+    }
+}
---
+++ src/test/java/hu/nje/gfekak/java/eloadas/JavaEloadasApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nje.gfekak.java.eloadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEloadasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
